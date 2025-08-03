7e17ee6feeb96e080fc6a85601d997cdb0a0c765
doickswag <robertdallas1337@gmail.com>
2025-08-03 14:37:56 +0300

    Revert "Merge pull request #16 from nvsreup/master"
    This reverts commit 1788c66e3562d95a1b97e97465bc43469d332cf8, reversing
    changes made to a1eaa09c6700d2adb50e1751469ae0a1b0bb2c58.

--- src/main/java/me/alpha432/oyvey/features/gui/items/Item.java
+++ src/main/java/me/alpha432/oyvey/features/gui/items/Item.java
@@ -1,8 +1,6 @@
 package me.alpha432.oyvey.features.gui.items;
 
 import me.alpha432.oyvey.features.Feature;
-import me.alpha432.oyvey.features.gui.Component;
-import me.alpha432.oyvey.features.gui.OyVeyGui;
 import net.minecraft.client.gui.DrawContext;
 
 import java.awt.*;
@@ -82,9 +80,5 @@ public class Item
 
     protected void drawString(String text, double x, double y, int color) {
         context.drawTextWithShadow(mc.textRenderer, text, (int) x, (int) y, color);
-    }
-
-    public boolean isHovering(int mouseX, int mouseY) {
-        return false;
     }
 }
